29918c4f154f99b7a3867cecec79c59720341440
[Jeramiah Coffey] <[jerrygcoffey@gmail.com]>
2021-09-08 13:05:20 -0500

    Initial Commit

---
+++ pa_jym1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pa_jym1
+//
+//  Created by Jeramiah Coffey on 9/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
